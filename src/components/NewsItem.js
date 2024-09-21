337a905dde3bf97e0db48438461e3ad14b50594e
Jayaant Kumar <jayantkumar7881@gmail.com>
2024-09-21 15:34:20 +0530

    created all components required

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,30 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const NewsItem = (props)=> {
-        let { title, description, imageUrl, newsUrl, author, date, source } = props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        position: 'absolute',
-                        right: '0'
-                    }
-                    }> 
-                        <span className="badge rounded-pill bg-danger"> {source} </span>
-                    </div>
-                    <img src={!imageUrl ? "https://tse3.mm.bing.net/th?id=OIF.HyqsVZfAGEn%2by%2fTaxMlyOg&pid=Api&P=0&h=180" : imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}  </h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                    </div>
-                </div>
-            </div>
-        )
-     
+export class NewsItem extends Component {
+  render() {
+    
+    return (
+      <div>
+        <div class="card" style={{width: "18rem"}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+    )
+  }
 }
 
 export default NewsItem
